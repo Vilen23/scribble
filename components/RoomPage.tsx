8dd41f962cfeba1c2a10e890879036ec80d1858e
Vilen23 <shivamlather5@gmail.com>
2024-04-29 21:18:10 +0530

    Adding websocket to it

--- components/RoomPage.tsx
+++ components/RoomPage.tsx
@@ -44,26 +44,42 @@ export default function RoomPage() {
       name: "",
     },
     roomUsers: [],
-  
   });
   const { roomid } = useParams();
-  const [roomUser,setRoomUser] = useRecoilState(roomUserAtom);
+  const [roomUser, setRoomUser] = useRecoilState(roomUserAtom);
 
   useEffect(() => {
     const fetchRoomInfo = async () => {
       const res = await axios.get(`/api/room/${roomid[0]}`);
       setRoom(res.data);
-      console.log(res.data)
+      console.log(res.data);
     };
     fetchRoomInfo();
   }, []);
 
+  useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:8080`);
+    ws.onmessage = function (event) {
+      console.log(event.data);
+      const message = event.data;
+      if (message.type === "userJoined") {
+        setRoom((prevRoom) => ({
+          ...prevRoom,
+          roomUsers: [...prevRoom.roomUsers, message.user],
+        }));
+      }
+      return () => {
+        ws.close();
+      };
+    };
+  }, []);
+
   const handleLeaveServer = async () => {
     try {
-        const res = await LeaveRoom(roomid[0],roomUser.id);
-        console.log(res);
-        setRoomUser(null);  
-        router.push('/')
+      const res = await LeaveRoom(roomid[0], roomUser.id);
+      console.log(res);
+      setRoomUser(null);
+      router.push("/");
     } catch (error) {
       console.log(error);
     }
@@ -84,14 +100,31 @@ export default function RoomPage() {
         />
       </div>
       <div className="flex justify-center w-full gap-10 mt-10">
-        <div className="w-[20%] border-r-2 border-t-2">{room.roomUsers.map((user)=>(
-            <div className="flex justify-start items-center text- md gap-2 py-2 pl-5 border-b-2" key={user.id}>
-                <Avatar>
-                    <AvatarFallback className="bg-black/10 text-sm" >{user.name[0]}</AvatarFallback>
-                </Avatar>
-                <p className={`${user.name===session.data?.user?.name?"text-black font-medium cursor-pointer":"cursor-pointer text-black/70 font-normal hover:text-black"}`}>{user.name===session.data?.user?.name?`${user.name}(You)`:`${user.name}`}</p>
+        <div className="w-[20%] border-r-2 border-t-2">
+          {room.roomUsers.map((user) => (
+            <div
+              className="flex justify-start items-center text- md gap-2 py-2 pl-5 border-b-2"
+              key={user.id}
+            >
+              <Avatar>
+                <AvatarFallback className="bg-black/10 text-sm">
+                  {user.name[0]}
+                </AvatarFallback>
+              </Avatar>
+              <p
+                className={`${
+                  user.name === session.data?.user?.name
+                    ? "text-black font-medium cursor-pointer"
+                    : "cursor-pointer text-black/70 font-normal hover:text-black"
+                }`}
+              >
+                {user.name === session.data?.user?.name
+                  ? `${user.name}(You)`
+                  : `${user.name}`}
+              </p>
             </div>
-        ))}</div>
+          ))}
+        </div>
         <div className="w-[50%] ">Hi2</div>
         <div className="w-[20%]">Hi3</div>
       </div>
--- lib/actions/joinroom.ts
+++ lib/actions/joinroom.ts
@@ -1,9 +1,8 @@
 "use server"
-
+import WebSocket from "ws";
 import { NEXT_AUTH } from "@/lib/auth"
 import prisma from "@/lib/db";
 import { getServerSession } from "next-auth"
-
 export const JoinRoom = async(name:string,password:string)=>{
     const session = await getServerSession(NEXT_AUTH);
     if(!session.user){return {error: 'You must be signed in to join a room'}}
@@ -20,5 +19,10 @@ export const JoinRoom = async(name:string,password:string)=>{
             roomId:room.id
         }
     })
+    const ws = new WebSocket('ws://localhost:8000');
+    ws.on('open', function open() {
+        ws.send(JSON.stringify({ type: 'userJoined', room }));
+        ws.close();
+    });
     return {room,roomUser}
 }
